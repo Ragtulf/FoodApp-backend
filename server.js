958523deb4e6fe88d38ebcf2c566dfee7f77f7a0
Ragna <ragnaulfars@gmail.com>
2020-06-15 13:28:22 +0200

    We get access to profile pic in recipes

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.get('/recipes', async (req, res) => {
   try {
     const recipes = await Recipe.find().populate({
       path: 'createdBy',
-    select: 'userName'
+      select: ['userName', 'profilePic']
     }).sort({ createdAt:'desc' }).exec()
     res.json(recipes)
   } catch (err) {
@@ -143,7 +143,7 @@ app.get('/recipes/:id', async (req, res) => {
   try {
     const recipe = await Recipe.findById(id).populate({
       path: 'createdBy',
-    select: 'userName'
+    select: ['userName', 'profilePic']
     })
     res.json(recipe)
   } catch (err) {
